62d1e1366428f4ca114f23e00dfe113eb3794725
Andersen Fan <as181920@gmail.com>
2026-05-06 12:50:16 +0800

    Feat: implement concierge_agent

--- lib/omni_article.rb
+++ lib/omni_article.rb
@@ -1,6 +1,12 @@
 require "omni_article/engine"
 
 module OmniArticle
+  def self.easy_agent_agents
+    [
+      Agents::ConciergeAgent
+    ]
+  end
+
   def self.easy_agent_tools
     [
       AgentTools::SearchArticlesTool
